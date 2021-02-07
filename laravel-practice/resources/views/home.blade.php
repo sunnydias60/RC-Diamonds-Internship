9b2ff36e73734eba7ad972b1dbe1df4a4df88f27
sunnydias60 <44764583+sunnydias60@users.noreply.github.com>
2021-02-07 20:40:14 +0530

    Add files via upload

---
+++ laravel-practice/resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    
+                    @if(session()->has('message'))
+                    <div class="alert alert-success">{{session()->get('message')}}</div>
+                    @elseif(session()->has('error'))
+                    <div class="alert alert-danger">{{session()->get('error')}}</div>
+                    @endif
+                    <form action="/upload" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <input type="file" name="image"/>
+                    <input type="submit" value="upload"/>
+                    </form>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
